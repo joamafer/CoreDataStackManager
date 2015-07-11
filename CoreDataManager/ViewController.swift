b5244078dfa7324adb6638544d0151e23918543b
Jose Antonio <joamafer@gmail.com>
2015-07-11 18:11:36 +0100

    Initial commit

---
+++ CoreDataManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataManager
+//
+//  Created by JOSE ANTONIO MARTINEZ FERNANDEZ on 11/07/2015.
+//  Copyright (c) 2015 jamartinez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
